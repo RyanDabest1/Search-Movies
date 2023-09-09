ab7a364d1f2f55dea47a80f89b37f3d961e5ca16
RyanDabest1 <ryanwinn231@gmail.com>
2023-09-09 08:57:03 +0630

    loading animation added

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const URL = "https://consumentapi.onrender.com//anime/zoro/";
+const URL = "https://consumentapi.onrender.com/anime/zoro/";
 let page = 1;
 let submitBtn = document.querySelector('.searchBtn')
 let animeList = document.querySelector('.animeList')
 let animeres = document.querySelector('.animeResults')
+let loading = document.querySelector('.loading')
 const options = {
     method : "GET",
 }
-
-
-const search = async function(url){
+const search = function(url){
+    loading.style.display = 'block'
+    setTimeout(async()=>{
     let response = await fetch(url, options);
     let result = await response.json();
-   for(let i = 0; i < result.results.length; i++){
+    for(let i = 0; i < result.results.length; i++){
         let title = result.results[i].title;
         let cardTemplate = document.createElement('div')
         cardTemplate.innerHTML = `
@@ -23,8 +24,14 @@ const search = async function(url){
     <a href="${result.results[i].url}" class="btn btn-primary">Watch now</a>
   </div>
 </div>`;
-        animeres.appendChild(cardTemplate)
+loading.style.display = 'none'
+animeres.appendChild(cardTemplate)
     } 
+    console.log('worked')
+}, 10000)
+    
+    
+  
 }
 
 submitBtn.addEventListener('click', () => { let child = animeres.lastElementChild; while(child){animeres.removeChild(child); child = animeres.lastElementChild}; search(`https://consumentapi.onrender.com/anime/zoro/${document.querySelector('#searchBox').value}?page=${page}`);console.log('clicked') })
